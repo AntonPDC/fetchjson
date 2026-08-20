64968984aa03c755d854857cf279a8767b272538
AntonPDC <antonpdecesare@gmail.com>
2026-08-20 14:15:28 -0400

    axios call

--- index.ts
+++ index.ts
@@ -1,7 +1,31 @@
 import axios from 'axios';
 
+interface Todo {
+    id: number;
+    title:string;
+    completed: boolean;
+}
+
+
 const url =  'https://jsonplaceholder.typicode.com/todos/1';
 
 axios.get(url).then(response => {
-    console.log(response.data)
+    const todo = response.data as Todo;
+
+    const id = todo.id
+    const title= todo.title
+    const completed = todo.completed
+
+
+    console.log(logTodo(id, title, completed))
+
 })
+
+
+const logTodo = (id: number, title: string, completed: boolean) => {
+    console.log(`
+    The todo with the ID: ${id}
+    With the title: ${title}
+    Is it completed? ${completed}
+`)
+}
